590ecf24917af499994dc83f774a2f767759f4bc
Jayasree1276 <saddalajayasree7@gmail.com>
2025-08-06 19:27:03 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const BASEURl=import.metaenv.BASE_URL;
+    const BASEURl=import.meta.env.BASE_URL;
     return (
       <>
         <header>
@@ -11,39 +11,39 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src={${BASEURL}1.jpeg} alt=''/>
+            <img src={`${BASE_URL}1.jpeg`} alt=''/>
             <label>image 1</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}2.jpeg} alt=''/>
+            <img src={`${BASE_URL}2.jpeg`} alt=''/>
             <label>image 2</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}3.jpeg} alt=''/>
+            <img src={`${BASE_URL}3.jpeg`} alt=''/>
             <label>image 3</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}4.jpeg} alt=''/>
+            <img src={`${BASE_URL}4.jpeg`} alt=''/>
             <label>image 4</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}5.jpeg} alt=''/>
+            <img src={`${BASE_URL}5.jpeg`} alt=''/>
             <label>image 5</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}6.jpeg} alt=''/>
+            <img src={`${BASE_URL}6.jpeg`} alt=''/>
             <label>image 6</label>
           </div>
         <div className='card'>
-            <img src={${BASEURL}7.jpeg} alt=''/>
+            <img src={`${BASEURL}7.jpeg} alt=''/>
             <label>image 7</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}8.jpeg} alt=''/>
+            <img src={${BASE_URL}8.jpeg`} alt=''/>
             <label>image 8</label>
           </div>
           <div className='card'>
-            <img src={${BASEURL}9.jpeg} alt=''/>
+            <img src={`${BASE_URL}9.jpeg`} alt=''/>
             <label>image 9</label>
           </div>
         </section>
